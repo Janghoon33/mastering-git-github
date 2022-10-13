9651a062488152db5234bbb2a43fb63cf9bb7ce8
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-10-13 15:18:25 +0900

    Edit index.js title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '전장훈' });
 });
 
 module.exports = router;

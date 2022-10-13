dc9b16f2f483390f6bc5f75c5b513151002d384a
김정훈 <104408864+kimjeonghoonn@users.noreply.github.com>
2022-10-13 15:16:56 +0900

    Edit index.js title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '전장훈' });
+  res.render('index', { title: '김정훈' });
 });
 
 module.exports = router;
